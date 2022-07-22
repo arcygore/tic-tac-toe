49a5389137cde394f15060a8b110ab8a504b21e1
arcygore <rcgore602@gmail.com>
2022-07-22 18:29:47 -0400

    Full game functionality present CSS could use work but not focus of this project yet JS fleshed out with user inputs and DOM manipulation

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # tic-tac-toe
+
+Topics Learned & Strengthened:
+- Module Pattern 
+- IIFEs
+- Factory Functions
+- Scope
+- DOM Manipulation
+- Array methods
+
+This is by far the most challenging project that I've had to complete for The Odin Project. I was able to get a 'functioning' game where the cells were auto generated with a "createBoard" function, and X and O could place their markers in succession to fill up the board, but the win/loss logic took me days. It's becoming increasingly apparent to me that array methods are going to be an incredibly important topic to understand, as the .every and .some methods were crucial to my solution of this project.
+
+Once the win/loss logic was completed, the rest of the project went by with relative ease. I'm not entirely sure that I put the "Player assignment" block within the right area (main game function), but I put a note in the code where I didn't figure out a way to assign players/clear cells but in their own function. It's the same thing that happened with creating a separate resetBoard() method within the gameBoard IIFE. Maybe it shouldn't be an IIFE for that since it will be invoked multiple times.
+
+All-in-all, the project took me just over a week to complete, with probably a total active time of about 15 hrs.
--- script.js
+++ script.js
@@ -5,22 +5,37 @@
 
 
 let turnCount = 1; 
+let xMarker = 'X';
+let oMarker = 'O';
+let currentMarker = xMarker;
 const winConditions = 
-[[0,1,2],
- [3,4,5],
- [6,7,8],
- [0,3,6],
- [1,4,7],
- [2,5,8],
- [0,4,8],
- [2,4,6]];
+                    [[0,1,2],
+                    [3,4,5],
+                    [6,7,8],
+                    [0,3,6],
+                    [1,4,7],
+                    [2,5,8],
+                    [0,4,8],
+                    [2,4,6]];
 
 const boardContainer = document.querySelector('.game-board');
 const boardCell = document.querySelectorAll('.cell');
+const winnerModal = document.querySelector('.winner-modal');
+const winnerText = document.querySelector('.winner-text');
+const rematchButton = document.querySelector('.rematch');
+const quitButton = document.querySelector('.quit');
+const overlay = document.querySelector('.overlay');
+const playerXName = document.querySelector('.playerXname');
+const playerOName = document.querySelector('.playerOname');
+const startButton = document.querySelector('.start');
+const resetButton = document.querySelector('.reset');
+const turn = document.querySelector('.turn');
+
 
 // Module Function 
 
 const gameBoard = (() => {
+
     const createBoard = () => {
         for (let i = 1; i <= 9; i++) {
             const cell = document.createElement('div');
@@ -30,57 +45,103 @@ const gameBoard = (() => {
             cell.style.alignItems = 'center';
             cell.style.justifyContent = 'center';
             cell.style.fontSize = '8rem';
-            // cell.addEventListener('click', () => { 
-            //     if (cell.innerText != '') return;
-            //     if (turnCount%2 === 1) cell.innerText = "X";
-            //     if (turnCount%2 === 0) cell.innerText = "O";
-            //     turnCount++;
-            // });
-            // cell.addEventListener('click', placeGameMarker(cell));
             boardContainer.appendChild(cell);
         }
     }
+
     return { createBoard };
 })();
 
-// const placeGameMarker = (cell) => { // I think this should be in an overall "Game" function
-//     if (cell.innerText != '') return;
-//     if (turnCount%2 === 1) cell.innerText = "X";
-//     if (turnCount%2 === 0) cell.innerText = "O";
-//     turnCount++;
-// };
+// Player Function - I don't know how to implement this with the way I've programmed
+// everything else in the game() function. I'm wanting to run through every cell and 
+// ensure they're cleared of data, but I create the game board in the game function.
 
-const player = () => {
+// const players = () => {
+//     let playerX = '';
+//     let playerO = '';
 
-}
+//     startButton.addEventListener('click', () => {
+//         playerX = playerXName.value;
+//         playerO = playerOName.value;
+//         game();
+//     })
+
+//     return { playerX , playerO }
+// }
+
 
 const game = () => {
     gameBoard.createBoard();
     const newCells = document.querySelectorAll('.cell');
 
+    const resetBoard = () => {
+        newCells.forEach((cell) => {
+            cell.innerText = '';
+            winnerText.innerText = '';
+            turnCount = 1;
+        }) 
+        currentMarker = xMarker;
+        winnerModal.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+
+    startButton.addEventListener('click', () => {
+        playerX = playerXName.value;
+        playerO = playerOName.value;
+        currentMarker = xMarker;
+        turn.innerText = `${playerX}'s turn!`;
+        resetBoard();
+    })
+
     boardContainer.addEventListener('click', (e) => {
         if (e.target.classList.contains('cell')) {
             if (e.target.innerText != '') return;
-            if (turnCount%2 === 1) e.target.innerText = "X";
-            if (turnCount%2 === 0) e.target.innerText = "O";
+            e.target.innerText = currentMarker;
             checkForWin();
-            turnCount++;
+            if (currentMarker === xMarker) {   
+                turn.innerText = `${playerO}'s turn!`;         
+                return currentMarker = oMarker;
+            }
+            else if (currentMarker === oMarker) {   
+                turn.innerText = `${playerX}'s turn!`;         
+                return currentMarker = xMarker;
+            }
         }
     })
 
+    rematchButton.addEventListener('click', resetBoard);
+    
+    quitButton.addEventListener('click', () => {
+        resetBoard();
+        turn.innerText = '';
+        playerOName.value = '';
+        playerXName.value = '';
+    })
+
     const checkForWin = () => {
-        // const newCells = document.querySelectorAll('.cell');
         if(winConditions.some((condition) => {
             return condition.every((currentVal) => {
-                return newCells[currentVal].innerText === 'X';
+                return newCells[currentVal].innerText === currentMarker;
             })
-        })) console.log("X is the winner!")
-        else if (winConditions.some((condition) => {
-            return condition.every((currentVal) => {
-                return newCells[currentVal].innerText === 'O';
-            })
-        })) console.log("O is the winner!")
+        })) {
+            if (currentMarker == xMarker) winnerText.innerText = `${playerX.toUpperCase()} WINS!`;
+            if (currentMarker == oMarker) winnerText.innerText = `${playerO.toUpperCase()} WINS!`;
+            winnerModal.classList.add('active');
+            overlay.classList.add('active');
+        }
     }
 
+    // const resetBoard = () => {
+    //     newCells.forEach((cell) => {
+    //         cell.innerText = '';
+    //         winnerText.innerText = '';
+    //         turnCount = 1;
+    //         winnerModal.classList.remove('active');
+    //         overlay.classList.remove('active');
+    //     }) 
+    //     currentMarker = xMarker;
+    // }
+
 }
+
 game();
